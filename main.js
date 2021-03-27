aefa41299f0ebe493cb117bba335dd5423f75ddd
999Rub <rubens.apprin73@gmail.com>
2021-03-27 17:01:11 +0100

    phaser 3.5

--- main.js
+++ main.js
@@ -21,6 +21,10 @@ const config = {
         preload: preload,
         create: create,
         update: update
+    },
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH
     }
   
     
